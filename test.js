e473731fb74b881fe833bdfe4d24fc552bcd5254
Max Peng <pengcheng95@gmail.com>
2018-02-05 00:01:37 -0500

    clean up test

--- test.js
+++ test.js
@@ -1,14 +1,7 @@
 const classes = require('./classes/index.js');
-const Node = classes.Node;
-const Tree = classes.Tree;
-const State = classes.State;
 const Board = classes.Board;
 const mcts = classes.MonteCarloTreeSearch;
-const selectPromisingNode = classes.selectPromisingNode;
-const UCT = classes.UCT;
-const expandNode = classes.expandNode;
-const backPropogation = classes.backPropogation;
-const simulateRandomPlayout = classes.simulateRandomPlayout;
+
 
 let simulateAiPlay = () => {
   let board = new Board();
